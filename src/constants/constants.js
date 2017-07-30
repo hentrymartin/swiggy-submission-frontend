7510c776b178df89f3fa3957fcd5270b318b934d
Hentry Martin <hentrymartin.p@gmail.com>
2017-07-30 22:22:27 +0530

    urls changed

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,6 @@
 export const Constants = {
-  // API_URL: 'http://ec2-34-210-106-157.us-west-2.compute.amazonaws.com:3001/api/',
-  API_URL: 'http://localhost:3001/api/',
-  // SOCKET_URL: 'http://ec2-34-210-106-157.us-west-2.compute.amazonaws.com:3001',
-  SOCKET_URL: 'http://localhost:3001',
+  API_URL: 'http://ec2-34-210-106-157.us-west-2.compute.amazonaws.com:3001/api/',
+  // API_URL: 'http://localhost:3001/api/',
+  SOCKET_URL: 'http://ec2-34-210-106-157.us-west-2.compute.amazonaws.com:3001',
+  // SOCKET_URL: 'http://localhost:3001',
 };
